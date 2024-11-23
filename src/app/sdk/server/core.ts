817fb217198b9f9a4c2e195e44ef3aff8ea7193a
Gabriel Pérez <gabriel@garox.org>
2024-11-23 14:54:21 -0300

    add supabase

--- src/app/sdk/server/core.ts
+++ src/app/sdk/server/core.ts
@@ -1,6 +1,24 @@
-export const processRequest = async (req: Request) => {
+import { z } from "zod";
+
+enum SupportedHTTPMethod {
+  GET = "GET",
+  POST = "POST",
+}
+
+const isSupportedHTTPMethod = (method: string): method is SupportedHTTPMethod =>
+  z.nativeEnum(SupportedHTTPMethod).safeParse(method).success;
+
+export const processRequest = async (req: Request, basePath?: string) => {
   const url = new URL(req.url.replace(/\/$/, ""));
   const { pathname } = url;
+
+  if (!isSupportedHTTPMethod(req.method)) {
+    throw new Error(`Unsupported HTTP method: ${req.method}`);
+  }
+
+  const method = req.method;
+
+  const finalPathname = basePath ? pathname.replace(basePath, "") : pathname;
 
   return pathname;
 };
---
+++ src/supabase/client.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
+import { env } from "@/env";
+
+export function createClient() {
+  return createBrowserClient(
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  );
+}
+
+export const supabaseClient = createClient();
