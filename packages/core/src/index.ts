5e248db637334d3256056f6e59c18156b844b75e
Gabriel Pérez <gabriel@garox.org>
2024-11-23 16:32:39 -0300

    use object cli

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export { createMercadoPagoIntegration } from "./integrations/mercadopago";
 export * from "./core";
 
 export type { PaymentIntent } from "./services/data.service";
+export type { SupportedCurrencies } from "./constants";
