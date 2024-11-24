9311d1e79724d6c5c69d5c8489d9a7651b23a236
Gabriel Pérez <gabriel@garox.org>
2024-11-23 22:27:43 -0300

    fix build

--- packages/sdk/src/supported-currencies.ts
+++ packages/sdk/src/supported-currencies.ts
@@ -3,6 +3,7 @@ import type { SupportedCurrencies as SupportedCurrenciesType } from "@rccpr/core
 export const SupportedCurrencies = {
   USD: "USD",
   CLP: "CLP",
+  MXN: "MXN",
 } as const satisfies Record<SupportedCurrenciesType, SupportedCurrenciesType>;
 
 type _SupportedCurrencies = typeof SupportedCurrencies;
