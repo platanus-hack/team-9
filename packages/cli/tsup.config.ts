7ca3da7b84f740d224d152fec3d98498c0194fb3
Diego García <dgarciavallejos@gmail.com>
2024-11-24 07:00:15 -0300

    refactor: update tsup

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts"],
+  entry: {
+    ".": "src/index.ts",
+  },
   format: ["cjs"],
   target: "node14",
-  minify: !options.watch,
   clean: true,
-  outDir: "dist",
 }));
